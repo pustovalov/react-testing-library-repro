dda8daaf8ba8fa485281d4d679717d72dedd02e6
Pavel Pustovalov <pustovalov.pavel@gmail.com>
2020-05-11 16:24:58 +0300

    remove done callback

--- src/App.test.js
+++ src/App.test.js
@@ -3,19 +3,17 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe(`main`, () => {
-  test('only this spec should be failed', async done => {
+  test('only this spec should be failed', async() => {
     const { findByTestId } = render(<App />)
     const test = await findByTestId('random-test-id')
-    expect(test).toBeInTheDocument()
 
-    done()
+    expect(test).toBeInTheDocument()
   })
 
-  test('should be success', async done => {
+  test('should be success', async() => {
     const { findByTestId } = render(<App />)
     const test = await findByTestId('learn-button')
 
     expect(test).toBeInTheDocument()
-    done()
   })
 })
